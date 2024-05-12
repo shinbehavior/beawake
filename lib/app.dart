e2eba1458a3a725c85a4878106c9ad29076dd0b6
Ilya K <hagforall@proton.me>
2024-05-12 19:09:23 +0200

    bunch of prints

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:beawake/screens/home_screen.dart';
 import 'package:beawake/screens/friends_screen.dart';
+import 'package:flutter/widgets.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -22,31 +23,39 @@ class _MyAppState extends State<MyApp> {
   void _checkLoginStatus() {
     var auth = FirebaseAuth.instance;
     auth.authStateChanges().listen((User? user) {
+      print("Auth state changed, user: $user");
       if (user == null) {
         setState(() {
           _children.clear();
           _children.add(SignInScreen());
+          print("No user found, showing SigninScreen");
         });
       } else {
         setState(() {
           _children..clear()
                    ..add(HomeScreen())
                    ..add(FriendsScreen());
+          print("User logged in, showing HomeScreen and FriendsScreen");
         });
       }
     });
   }
 
   void onTabTapped(int index) {
+    print("Tab tapped: $index");
     setState(() {
       _currentIndex = index;
+      print("Current index set to: $_currentIndex");
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print("Building Scaffold with current index: $_currentIndex");
+    print("Children count: ${_children.length}");
     return MaterialApp(
       home: Scaffold(
+        key: ValueKey<int>(_children.length),
         body: IndexedStack(
           index: _currentIndex,
           children: _children,
