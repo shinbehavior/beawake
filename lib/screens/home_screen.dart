18794029111558953563d64e04c9507082a316aa
Ilya Kuhta <ilya.kuhta@nxtech.no>
2024-06-15 14:05:10 +0200

    fix

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,11 +2,21 @@
 import 'package:flutter/material.dart';
 import 'package:beawake/widgets/toggle_main_button.dart';
 import 'package:beawake/widgets/event_list.dart';
+import 'package:beawake/widgets/todo_list_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
   final String userId;
 
   const HomeScreen({Key? key, required this.userId}) : super(key: key);
+
+  void _openTodoListDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return TodoListDialog(userId: userId);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +33,15 @@ class HomeScreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const AnimatedToggleButton(),
               const SizedBox(height: 20),
               Expanded(child: EventList()),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _openTodoListDialog(context),
+                child: const Text('Add TODO List'),
+              ),
             ],
           ),
         ),
